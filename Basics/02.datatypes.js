9d4257e4f21cec3cc6da6ea1be09bab7b3bf4e50
nirbhayrzd <130433735+nirbhayrzd@users.noreply.github.com>
2024-02-12 08:17:12 +0000

    datatypes

---
+++ Basics/02.datatypes.js
@@ -0,0 +1,32 @@
+"use strict" //it uses in newer version
+
+//alert(3+3) // we are using nodejs not browser
+
+// make sure code readability is high
+
+console.log(3
+    +3)
+
+console.log("nirbhay")
+
+let name =  "nirbhay"
+let age = 22
+let isloggedin = true
+
+/* 
+ types of datatypes
+
+ 1. number
+ 2. string => ""
+ 3. boolean
+ 4. null
+ 5. undefined
+ 6.symbol => unique
+
+*/
+
+// 7. object
+
+console.log(typeof age);
+console.log(typeof null);
+
