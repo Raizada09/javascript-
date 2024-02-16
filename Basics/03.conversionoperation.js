7fb56f21efb9f88c5556d0d11923f2ba4f3d982f
nirbhayrzd <130433735+nirbhayrzd@users.noreply.github.com>
2024-02-16 06:37:16 +0000

    discussion on datatypes

---
+++ Basics/03.conversionoperation.js
@@ -0,0 +1,51 @@
+/*
+let score = 33
+console.log(score)
+console.log(typeof score)
+*/
+
+//let score = "33"
+//console.log(score)
+//console.log(typeof score)
+
+let score = "nirbhay"
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+// some results of conversion in numbers
+// "33" => 33
+// "33abc" => NaN(not a number)
+// true => 1 and false => 0
+
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanisLoggedIn);
+
+//console.log(typeof booleanisLoggedIn);
+
+// some results in boolean
+
+// 0 => false , 1 => true, "" => false , "anything inside"=> true
+
+let someNumber = 12
+
+let stringNumber = String(someNumber)
+
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+
+// **************************OPERATIONS************************
+
+
+
+
+
+
