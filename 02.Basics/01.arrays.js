885024b5ada9cfb890c474c169091c49dcd21f0c
nirbhayrzd <130433735+nirbhayrzd@users.noreply.github.com>
2024-04-25 09:02:54 +0000

    array pract

---
+++ 02.Basics/01.arrays.js
@@ -0,0 +1,36 @@
+// arrays
+
+const myArr = [0,1,2,3,4]
+const myHeroes = ["Thor", "Captain_America", "Hulk", "Loki"]
+
+const myArr2 = new Array(1,2,3,4)
+
+//console.log(myArr[1]);
+
+// array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3))
+
+const newArr = myArr.join()
+
+// console.log(myyArr)
+// console.log(newArr)
+
+
+
+// slice & splice
+
+console.log("A ", myArr)
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1)
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2)
