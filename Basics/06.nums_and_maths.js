3e490e2bbeb9b5ee9008b61f17f968ef10fc2454
Raizada09 <120782583+Raizada09@users.noreply.github.com>
2024-08-31 11:42:37 +0000

    cool

--- Basics/06.nums_and_maths.js
+++ Basics/06.nums_and_maths.js
@@ -2,8 +2,20 @@ const score = 400
 console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(1));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.895
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ***************** Maths ********************
+
+
+
+
 
